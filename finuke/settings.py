422fb49ee7aaf3c8cf968ca58a973bd912fe7734
Guillaume Royer <perso@guilro.com>
2018-03-09 15:05:11 +0100

    use base_url for statics

--- finuke/settings.py
+++ finuke/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/' + (BASE_URL or '') + 'static/'
 STATIC_ROOT = os.environ.get('STATIC_ROOT')
 
 # Django webpack loader config
