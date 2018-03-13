805de0ac9b942a1d91b71c8486379e716aa53194
Arthur Cheysson <arthur@cheysson.fr>
2018-03-13 13:46:02 +0100

    Add user and group to admin panel

--- finuke/admin.py
+++ finuke/admin.py
@@ -1,8 +1,13 @@
 from django.contrib.admin import AdminSite
-
+from django.contrib.auth import admin as auth_admin
 
 class FinukeAdmin(AdminSite):
     site_header = 'Finuke'
 
 
 admin_site = FinukeAdmin(name='finuke_admin')
+
+
+# register auth model admins
+admin_site.register(auth_admin.Group, auth_admin.GroupAdmin)
+admin_site.register(auth_admin.User, auth_admin.UserAdmin)
