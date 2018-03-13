556dcf2f80584f93bb4d96dc117d1c07fe13187d
Guillaume Royer <perso@guilro.com>
2018-03-13 18:57:48 +0100

    export operators can write to stdout

--- bureaux/management/commands/export_operators.py
+++ bureaux/management/commands/export_operators.py
@@ -1,5 +1,6 @@
 import csv
 
+import sys
 from django.core.management import BaseCommand
 
 from bureaux.models import BureauOperator
@@ -9,11 +10,11 @@ class Command(BaseCommand):
     help = 'Importer une liste de mail d\'operateurs'
 
     def add_arguments(self, parser):
-        parser.add_argument('filename')
         parser.add_argument('host')
+        parser.add_argument('-o', action='store')
 
     def handle(self, *args, **options):
-        with open(options['filename'], 'w', encoding='utf-8-sig') as file:
+        with (options.get('o') is not None and open(options['o'], 'w')) or sys.stdout as file:
             writer = csv.writer(file)
             for operator in BureauOperator.objects.filter(login_links__valid=True):
                 writer.writerow([operator, options['host'] + operator.login_links.filter(valid=True).first().get_absolute_url()])
