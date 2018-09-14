d42ca629a83e18edd1d0ed8642441c2baf39f410
Guillaume Royer <perso@guilro.com>
2018-09-14 17:18:10 +0200

    add labels to message level field

--- custom_messages/models.py
+++ custom_messages/models.py
@@ -1,9 +1,16 @@
-from django.contrib.messages.constants import INFO
+from django.contrib.messages.constants import INFO, SUCCESS, WARNING, ERROR
 from django.db import models
 
 
 class PersistantMessage(models.Model):
-    level = models.PositiveSmallIntegerField(default=INFO)
+    levels = (
+        (INFO, "Info"),
+        (SUCCESS, "Success"),
+        (WARNING, "Warning"),
+        (ERROR, "Error"),
+    )
+
+    level = models.PositiveSmallIntegerField(default=INFO, choices=levels)
     text = models.TextField(blank=False)
     enabled = models.BooleanField(default=True)
     url_name = models.CharField(max_length=255, blank=True)
