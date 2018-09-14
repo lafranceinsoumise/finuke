5a83df38cd6b129e9e9b1ad64d821017b631dfc0
Guillaume Royer <perso@guilro.com>
2018-09-14 17:30:56 +0200

    change message admin

--- custom_messages/admin.py
+++ custom_messages/admin.py
@@ -6,6 +6,5 @@ from .models import PersistantMessage
 
 @admin.register(PersistantMessage, site=admin_site)
 class PersistantMessageAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'level', 'enabled', 'url_name')
-    fields = ('pk', 'level', 'text', 'enabled', 'url_name')
-    readonly_fields = ('pk',)
+    list_display = ('pk', 'level', 'text', 'enabled', 'url_name')
+    fields = ('level', 'text', 'enabled', 'url_name')
